ce71f9ce2a966748d2250224e70dd2e3b6b139dd
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2023-01-01 07:49:38 -0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,31 @@
+import warnings
+import cv2
+import os
+from TVFF import torVideoFormat
+
+warnings.filterwarnings("ignore")
+
+
+def isVidFile(file):
+    try:
+        video = cv2.VideoCapture(file)
+        if video.isOpened():
+            video.release()
+            return True
+        else:
+            video.release()
+            return False
+    except:
+        return False
+
+
+v = os.listdir()
+for file in v:
+    if os.path.isfile(file):
+        if isVidFile(file):
+            got = torVideoFormat(file, "None", "true", output_dir="pngs")
+            if got['code'] == -1:
+                print(f"{file}:-:{got['path']}")
+                exit()
+            else:
+                print(f"couldnt make png for {file}")
